dfd2f51255abb6e1895ffe42e762979feeb1da03
unknown <akhilkashyap16@gmail.com>
2024-01-28 00:02:24 +0530

    Add function to create wallpaper

--- app.py
+++ app.py
@@ -4,29 +4,40 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
+
+
+def create_wallpaper(user_preferences):
+    open_ai_api_key = os.getenv('OPEN_AI_API_KEY')
+    client = OpenAI(api_key=open_ai_api_key)
+    theme = user_preferences['theme']
+    color = user_preferences['color']
+    additional = user_preferences['additional']
+    response = client.images.generate(
+    model="dall-e-2",
+    prompt=f"Generate a ultra high-quality wallpaper image of the theme {theme} in the color of {color} with the presence of {additional}. Make the image appear picturesque and alluring.",
+    size="1024x1024",
+    quality="standard",
+    n=1,
+    )
+    image_url = response.data[0].url
+    print(response)
+    return image_url
+
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    image_url = request.args.get('image_url')
+    return render_template('index.html', image_url=image_url)
 
 @app.route('/generate', methods=['POST'])
 def generate_wallpaper():
     # Retrieve user preferences from the form submission
     user_preferences = request.form
-    return redirect(url_for('index'))
+    image_url = create_wallpaper(user_preferences)
+    return redirect(url_for('index', image_url=image_url))
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-open_ai_api_key = os.getenv('OPEN_AI_API_KEY')
-client = OpenAI(api_key=open_ai_api_key)
-
-response = client.images.generate(
-  model="dall-e-3",
-  prompt="a white siamese cat",
-  size="1024x1024",
-  quality="standard",
-  n=1,
-)
-
-image_url = response.data[0].url
